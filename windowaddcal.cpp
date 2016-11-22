6732473d73ecdd1f37ffdbbec139f3c5409b9612
Lucas Zanella <me@lucaszanella.com>
2016-11-22 21:38:34 -0200

    adds dialog

---
+++ windowaddcal.cpp
@@ -0,0 +1,6 @@
+#include "windowaddcal.h"
+
+WindowAddCal::WindowAddCal()
+{
+
+}
---
+++ windowaddcal.h
@@ -0,0 +1,11 @@
+#ifndef WINDOWADDCAL_H
+#define WINDOWADDCAL_H
+
+
+class WindowAddCal
+{
+public:
+    WindowAddCal();
+};
+
+#endif // WINDOWADDCAL_H
